7031cbfcac1bb96fe95d88363d764dd24486c07d
Shreyank B R <157615323+shreyankbr@users.noreply.github.com>
2025-11-21 01:29:53 +0530

    Update firebase-config.js

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -1,9 +1,10 @@
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAWROptizjRhMfiRvA6_K4g99ID_rKSUq8",
-    authDomain: "fakebuster-8464c.firebaseapp.com",
-    projectId: "fakebuster-8464c",
-    storageBucket: "fakebuster-8464c.firebasestorage.app",
-    messagingSenderId: "705533861475",
-    appId: "1:705533861475:web:802d458d26df38da5be256"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
